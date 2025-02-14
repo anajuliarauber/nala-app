111d270d53f276a396cedda3f53237a25c6393e6
anajuliarauber <anajuliarauber@hotmail.com>
2025-02-14 11:30:27 -0300

    feat: enhance Flow component to fetch positions and relations, and create new nodes and edges

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -10,53 +10,58 @@ import {
   Connection,
   addEdge,
   useNodesState,
+  Edge,
 } from '@xyflow/react';
 import * as Toolbar from '@radix-ui/react-toolbar';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 
 import { Card } from '@/components/Card';
+import { createRelation } from '@/services/relations';
+import { createPosition } from '@/services/positions';
+import { getPositionsAsNodes, getRelationsAsEdges } from '../utils';
 
 const NODE_TYPES = {
   card: Card,
 };
 
-const INITIAL_NODES: Node[] = [
-  {
-    id: '1',
-    type: 'card',
-    position: {
-      x: 550,
-      y: 100,
-    },
-    data: {
-      isInitialNode: true,
-    },
-  },
-];
-
 export function Flow() {
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node>([]);
 
-  const onConnect = useCallback((connection: Connection) => {
-    return setEdges((edges) => addEdge(connection, edges));
-  }, []);
+  useEffect(() => {
+    const fetchRelations = async () => {
+      const result = await getRelationsAsEdges();
+      setEdges(result);
+    };
+
+    const fetchPositions = async () => {
+      const result = await getPositionsAsNodes();
+      setNodes(result);
+    };
+    fetchPositions();
+    fetchRelations();
+  }, [setEdges, setNodes]);
+
+  const onConnect = useCallback(
+    (connection: Connection) => {
+      const newEdge = addEdge(connection, edges);
+      setEdges(newEdge);
+      createRelation(newEdge[newEdge.length - 1]);
+    },
+    [edges, setEdges]
+  );
+
   function addNode() {
-    setNodes((nodes) => [
-      ...nodes,
-      {
-        id: '3',
-        type: 'card',
-        position: {
-          x: 500,
-          y: 500,
-        },
-        data: {
-          isInitialNode: false,
-        },
-      },
-    ]);
+    const newNode: Node = {
+      id: `${nodes.length + 1}`,
+      type: 'card',
+      position: { x: 500, y: 500 },
+      data: { label: `Position ${nodes.length + 1}` },
+    };
+    setNodes([...nodes, newNode]);
+    createPosition(newNode);
   }
+
   return (
     <div className="h-[70vh] w-[70vw] border relative">
       <ReactFlow
@@ -70,14 +75,13 @@ export function Flow() {
         className="bg-gray-200"
       >
         <Background gap={20} size={2} bgColor="#f7f2f2" color="#ccc" />
-        <Controls position='bottom-right' />
+        <Controls position="bottom-right" />
       </ReactFlow>
 
       <Toolbar.Root className="absolute top-4 right-4 bg-black text-white rounded-xl shadow-lg px-8 overflow-hidden">
-        <Toolbar.Button
-          className="rounded-sm py-3"
-          onClick={addNode}
-        >Create a position</Toolbar.Button>
+        <Toolbar.Button className="rounded-sm py-3" onClick={addNode}>
+          Create a position
+        </Toolbar.Button>
       </Toolbar.Root>
     </div>
   );
